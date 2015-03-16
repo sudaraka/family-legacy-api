7ab1c748d62aa3ae43f624f62ebc710c635aadf4
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2015-03-17 01:13:46 +0530

    Generate coverage report after test run

--- test.py
+++ test.py
@@ -3,8 +3,17 @@
 test.py: run available tests
 """
 
+import coverage
+
+
+_coverage = coverage.coverage()
+_coverage.start()
+
 import unittest
 
 unittest.TextTestRunner(verbosity=1).run(
     unittest.TestLoader().discover('src/tests/')
 )
+
+_coverage.stop()
+_coverage.report()
