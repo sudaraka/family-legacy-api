380b548f371bf54eeb9a97374bb93aa490390c06
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2015-03-16 13:52:07 +0530

    Create database (schema) when development server starts

--- server.py
+++ server.py
@@ -5,9 +5,13 @@ server.py: runs development server
 
 import os
 
-from src.app import create_app
+from src.app import create_app, db
+
 
 if '__main__' == __name__:
     app = create_app(os.environ.get('FLASK_CONFIG', 'dev'))
+
+    with app.app_context():
+        db.create_all()
 
     app.run()
