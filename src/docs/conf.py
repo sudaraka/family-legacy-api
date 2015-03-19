28e1f1a777fd654e56e29efef88e5a9bc6cb7c05
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2015-03-19 16:55:27 +0530

    Remove sidebar from documents

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -160,7 +160,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
-    '**': ['navigation.html']
+    '**': []
 }
 
 # Additional templates that should be rendered to pages, maps page names to
