749a76a51fdc6810de53aaee8447ed3137b93c91
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2015-03-24 14:30:19 +0530

    Model: Person - first draft

---
+++ src/app/models/person.py
@@ -0,0 +1,25 @@
+""" Person data model """
+
+from .. import db
+
+person_status = [
+    'UNPAID',    # Person registered, but not paid for access to create
+    'DISABLED',  # Disabled by administrator or system
+    'ACTIVE',    # Person active
+    'DECEASED'   # Person was marked by caretaker and confirmed as deceased
+]
+
+
+class Person(db.Model):
+    """ Person data model declarations """
+
+    __tablename__ = 'flapi_persons'
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Enum(*person_status), default='UNPAID',
+                       nullable=False, index=True)
+
+    first_name = db.Column(db.String(32), nullable=False)
+    last_name = db.Column(db.String(32), nullable=False)
+    email = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    avatar = db.Column(db.String(128))
