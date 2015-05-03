27e24293a8b7d0e9035014e3cd98f40f93173da1
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2015-05-03 16:47:29 +0530

    Added HTTP ETag support

--- src/app/decorators.py
+++ src/app/decorators.py
@@ -1,8 +1,9 @@
 """ Function decorators """
 
 import functools
+import hashlib
 
-from flask import jsonify
+from flask import jsonify, make_response, request
 
 
 def json(f):
@@ -39,6 +40,56 @@ def json(f):
 
         if headers is not None:
             response.headers.extend(headers)
+
+        return response
+
+    return wrapped
+
+
+def etag(f):
+    """ Add ETag HTTP header to the response of route """
+
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        """ wrap route function """
+
+        rv = make_response(f(*args, **kwargs))
+
+        # ETag is only supported in responses to HTTP GET and HEAD requests
+        if request.method not in ['HEAD', 'GET']:
+            return rv
+
+        # Make sure we have a valid Flask response object to work with
+        response = make_response(rv)
+
+        # Don't generate Etag for errors
+        if 200 != response.status_code:
+            return response
+
+        _etag = '"' + hashlib.md5(response.get_data()).hexdigest() + '"'
+        response.headers['ETag'] = _etag
+
+        if_match = request.headers.get('If-Match')
+        if_none_match = request.headers.get('If-None-Match')
+
+        if if_match:
+            # Only return the actual response if the generated ETag IS IN the
+            # list of ETags sent in request headers.
+
+            etag_list = [t.strip() for t in if_match.split(',')]
+
+            if _etag not in etag_list and '*' not in etag_list:
+                response = jsonify({})
+                response.status_code = 412  # Precondition Failed
+        elif if_none_match:
+            # Only return the actual response if the generated ETag IS NOT IN
+            # the list of ETags sent in request headers.
+
+            etag_list = [t.strip() for t in if_none_match.split(',')]
+
+            if _etag in etag_list or '*' in etag_list:
+                response = jsonify({})
+                response.status_code = 304  # Not Modified
 
         return response
 
---
+++ src/tests/test_api_caching.py
@@ -0,0 +1,76 @@
+""" Tests for API caching functions """
+
+from . import BaseCase
+
+
+class CachingTest(BaseCase):
+    """ Caching test case """
+
+    def test_no_etag_in_404(self):
+        """ 404 (or any error) response must not have an Etag """
+
+        response = self.client.get('/persons/1')
+
+        self.assert404(response)
+
+        self.assertNotIn('ETag', response.headers)
+
+    def test_etag_match(self):
+        """
+        With If-Match header set only response with matching ETag should return
+        """
+
+        self.create_persons()
+
+        response = self.client.get('/persons/1', headers={
+            'If-Match': '"e8259b42dfd55a8130662194107b2929"'
+        })
+
+        self.assert200(response)
+
+        response = self.client.get('/persons/1', headers={
+            'If-Match': '"bad-tag"'
+        })
+
+        self.assertEqual(412, response.status_code)
+
+    def test_etag_none_match(self):
+        """
+        With If-None-Match header set only response with non-matching ETag
+        should return
+        """
+
+        self.create_persons()
+
+        response = self.client.get('/persons/1', headers={
+            'If-None-Match': '"e8259b42dfd55a8130662194107b2929"'
+        })
+
+        self.assertEqual(304, response.status_code)
+
+        response = self.client.get('/persons/1', headers={
+            'If-None-Match': '"bad-tag"'
+        })
+
+        self.assert200(response)
+
+        response = self.client.get('/persons/2', headers={
+            'If-None-Match': '"e8259b42dfd55a8130662194107b2929"'
+        })
+
+        self.assert200(response)
+
+    def create_persons(self):
+        """ Create a temporary test record """
+
+        self.create_resource('/persons/', {
+            'first_name': 'P1 First Name',
+            'last_name': 'P1 Last Name',
+            'email': 'P1 Email'
+        })
+
+        self.create_resource('/persons/', {
+            'first_name': 'P2 First Name',
+            'last_name': 'P2 Last Name',
+            'email': 'P2 Email'
+        })
